11f996a4b289adcbd72dcbcdc3c49b6505250986
keruzam <mazurek.zg@gmail.com>
2024-10-20 23:49:43 +0200

    DevConfig test

--- src/main/java/pl/keruzam/spring_boot2024/config/DevConfig.java
+++ src/main/java/pl/keruzam/spring_boot2024/config/DevConfig.java
@@ -1,14 +1,26 @@
 package pl.keruzam.spring_boot2024.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.keruzam.spring_boot2024.TestBean;
 
 @Configuration
+@ConfigurationProperties(prefix = "dev")
 public class DevConfig {
+
+	String text;
 
 	@Bean
 	public TestBean testBean() {
-		return new TestBean("new text2");
+		return new TestBean(text);
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
 	}
 }
